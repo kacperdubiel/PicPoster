0c06a2aeffdebfbe419d0d065562f8af724cb3bd
kacperdubiel <kacper.dubiel13@gmail.com>
2021-05-17 21:39:25 +0200

    User API

---
+++ picposter/src/main/java/com/picposter/api/UserController.java
@@ -0,0 +1,73 @@
+package com.picposter.api;
+
+import com.picposter.domain.User;
+import com.picposter.service.UserServiceAPI;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.lang.NonNull;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+public class UserController {
+    private final UserServiceAPI userService;
+
+    @Autowired
+    public UserController(@Qualifier("userService") UserServiceAPI userService){
+        this.userService = userService;
+    }
+
+    @RequestMapping(path = "users/{id}", method = RequestMethod.GET)
+    public ResponseEntity<User> getUserById(@PathVariable("id") UUID id){
+        User userResult = userService.getUserById(id);
+        if(userResult == null)
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        else
+            return new ResponseEntity<>(userResult, HttpStatus.OK);
+    }
+
+    @RequestMapping(path = "users/login/{login}", method = RequestMethod.GET)
+    public ResponseEntity<User> getUserByLogin(@PathVariable("login") String login){
+        User userResult = userService.getUserByLogin(login);
+        if(userResult == null)
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        else
+            return new ResponseEntity<>(userResult, HttpStatus.OK);
+    }
+
+    @RequestMapping(path = "users/email/{email}", method = RequestMethod.GET)
+    public ResponseEntity<User> getUserByEmail(@PathVariable("email") String email){
+        User userResult = userService.getUserByEmail(email);
+        if(userResult == null)
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        else
+            return new ResponseEntity<>(userResult, HttpStatus.OK);
+    }
+
+    @RequestMapping(path = "users", method = RequestMethod.POST)
+    public ResponseEntity<User> addUser(@RequestBody @NonNull User user){
+        User userResult = userService.addUser(user);
+        return new ResponseEntity<>(userResult, HttpStatus.CREATED);
+    }
+
+    @RequestMapping(path = "users/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<Boolean> deleteUserById(@PathVariable("id") UUID id){
+        boolean deleteResult = userService.deleteUserById(id);
+        if(deleteResult)
+            return new ResponseEntity<>(true, HttpStatus.OK);
+        else
+            return new ResponseEntity<>(false, HttpStatus.NOT_FOUND);
+    }
+
+    @RequestMapping(path = "users", method = RequestMethod.PUT)
+    public ResponseEntity<User> updateUser(User user){
+        User userResult = userService.updateUser(user);
+        if(userResult != null)
+            return new ResponseEntity<>(userResult, HttpStatus.OK);
+        else
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+    }
+}
---
+++ picposter/src/main/java/com/picposter/service/UserService.java
@@ -0,0 +1,76 @@
+package com.picposter.service;
+
+import com.picposter.domain.Post;
+import com.picposter.domain.User;
+import com.picposter.repository.PostDAO;
+import com.picposter.repository.UserDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Service("userService")
+public class UserService implements UserServiceAPI{
+    private UserDAO userDAO;
+
+    @Autowired
+    public UserService(@Qualifier("userDAO") UserDAO userDAO){
+        this.userDAO = userDAO;
+    }
+
+    @Override
+    public User getUserById(UUID userId) {
+        return userDAO.findById(userId).orElse(null);
+    }
+
+    @Override
+    public User getUserByLogin(String userLogin) {
+        List<User> allUsers = userDAO.findAll();
+        Optional<User> foundUser = allUsers.stream().filter(user -> user.getLogin().equals(userLogin)).findFirst();
+        return foundUser.orElse(null);
+    }
+
+    @Override
+    public User getUserByEmail(String userEmail) {
+        List<User> allUsers = userDAO.findAll();
+        Optional<User> foundUser = allUsers.stream().filter(user -> user.getEmail().equals(userEmail)).findFirst();
+        return foundUser.orElse(null);
+    }
+
+    @Override
+    public User addUser(User user) {
+        user.setId(UUID.randomUUID());
+        while(userDAO.findById(user.getId()).isPresent())
+            user.setId(UUID.randomUUID());
+        return userDAO.save(user);
+    }
+
+    @Override
+    public boolean deleteUserById(UUID userId) {
+        if(userDAO.findById(userId).isPresent()){
+            userDAO.deleteById(userId);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public User updateUser(User updatedUser) {
+        User resultUser = userDAO.findById(updatedUser.getId()).orElse(null);
+        if(resultUser != null){
+            resultUser.setLogin(updatedUser.getLogin());
+            resultUser.setPassword(updatedUser.getPassword());
+            resultUser.setEmail(updatedUser.getEmail());
+            resultUser.setLastLoginDate(updatedUser.getLastLoginDate());
+            resultUser.setCreatedDate(updatedUser.getCreatedDate());
+            resultUser.setDescription(updatedUser.getDescription());
+            resultUser.setProfileImagePath(updatedUser.getProfileImagePath());
+            return userDAO.save(resultUser);
+        }
+        return null;
+    }
+}
