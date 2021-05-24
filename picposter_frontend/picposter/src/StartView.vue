6d3fa7857e4604bdf4ca459a08e8007cce1cb815
majajurenczyk <majka.jurenczyk@gmail.com>
2021-05-24 17:22:23 +0200

    start view init

---
+++ picposter_frontend/picposter/src/StartView.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="start-screen">
+    <h1>PIC POSTER </h1>
+    <login-component></login-component>
+    <register-component></register-component>
+  </div>
+</template>
+
+<script>
+
+import LoginComponent from './components/LoginComponent.vue'
+import RegisterComponent from './components/RegisterComponent.vue'
+
+export default {
+  name: 'StartView',
+  components: {
+    LoginComponent,
+    RegisterComponent
+  }
+}
+</script>
+
+<style>
+#start-screen {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
