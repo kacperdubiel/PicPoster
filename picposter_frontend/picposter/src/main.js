f1ecb51471a4d5c2ae1eb4f3ca3b97b8dc04be3f
majajurenczyk <majka.jurenczyk@gmail.com>
2021-05-24 18:07:55 +0200

    add bootstrap

--- picposter_frontend/picposter/src/main.js
+++ picposter_frontend/picposter/src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import StartView from './StartView.vue'
+import BootstrapVue from 'bootstrap-vue'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.use(BootstrapVue)
 
 new Vue({
   render: h => h(StartView),
