36c328e1a14b467886fa7db0235d206e763daec4
majajurenczyk <majka.jurenczyk@gmail.com>
2021-05-25 14:44:28 +0200

    merge

--- picposter_frontend/picposter/src/main.js
+++ picposter_frontend/picposter/src/main.js
@@ -1,11 +1,28 @@
 import Vue from 'vue'
 import ProfileView from './ProfileView'
+import StartView from './StartView'
+import WallView from './WallView'
 //import StartView from './StartView.vue'
 //import BootstrapVue from 'bootstrap-vue'
+
+const routes = {
+  '/': StartView,
+  '/profile': ProfileView,
+  '/wall' : WallView
+}
 
 Vue.config.productionTip = false;
 //Vue.use(BootstrapVue)
 
 new Vue({
-  render: h => h(ProfileView),
+  el: '#app',
+  data: {
+    currentRoute: window.location.pathname
+  },
+  computed: {
+    ViewComponent () {
+      return routes[this.currentRoute]
+    }
+  },
+  render (h) { return h(this.ViewComponent) }
 }).$mount('#app')
